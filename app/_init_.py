41d5bd1e6324840cfa7ae29e6495ff288a68766c
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-14 15:01:24 +0000

    Update _init_.py

--- app/_init_.py
+++ app/_init_.py
@@ -1,1 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+login_manager = LoginManager()
+migrate = Migrate()
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
+    login_manager.login_view = "auth.login"
+
+    from app.routes.auth_routes import auth
+    from app.routes.dashboard_routes import dashboard
+
+    app.register_blueprint(auth)
+    app.register_blueprint(dashboard)
+
+    return app
