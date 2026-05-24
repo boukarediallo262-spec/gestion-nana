9d4c6602f9028b92787b2ff75ca73848e3660a13
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-24 10:01:54 +0000

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
 
-# =========================
-# HOME
-# =========================
 @dashboard_bp.route("/")
 def home():
+    return redirect("/login")
+
+
+@dashboard_bp.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/login")
+
     return render_template("dashboard/index.html")
