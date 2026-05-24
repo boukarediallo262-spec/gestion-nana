93c22f49901346c346812981a292422009e24953
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-24 20:27:05 +0000

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -1,10 +1,27 @@
 from flask import Blueprint, render_template
+from app.models.models import Produit, Facture, Depense
 
-dashboard_bp = Blueprint(
-    "dashboard",
-    __name__
-)
+dashboard_bp = Blueprint("dashboard", __name__)
 
 @dashboard_bp.route("/")
 def home():
-    return render_template("dashboard.html")
+
+    total_produits = Produit.query.count()
+    total_factures = Facture.query.count()
+
+    depenses = Depense.query.all()
+
+    total_depenses = 0
+
+    for d in depenses:
+        total_depenses += d.montant
+
+    abonnement = "Actif"
+
+    return render_template(
+        "dashboard/index.html",
+        total_produits=total_produits,
+        total_factures=total_factures,
+        total_depenses=total_depenses,
+        abonnement=abonnement
+    )
