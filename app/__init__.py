77a3acb67ec0793e78277ff889a5ca1c033f4364
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-24 09:34:51 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from app.models.models import db
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     from app.routes.dashboard_routes import dashboard_bp
     app.register_blueprint(dashboard_bp)
