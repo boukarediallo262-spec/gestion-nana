f7835fc4d2100480c3be64f8bc7f84f46df11491
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-25 10:58:33 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,7 @@ import os
 db = SQLAlchemy()
 
 def create_app():
-    app.register_blueprint(abonnement_bp)
-
+    
     app = Flask(
         __name__,
         template_folder="templates",
@@ -17,6 +16,8 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "secret")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.register_blueprint(abonnement_bp)
+
 
     db.init_app(app)
 
