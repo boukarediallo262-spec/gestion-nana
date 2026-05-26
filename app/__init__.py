655ca9c5588b5076ea2fceefbce320a7a5c555c0
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-26 11:54:18 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,12 +19,14 @@ def create_app():
     from app.routes.produit_routes import produit_bp
     from app.routes.abonnement_routes import abonnement_bp
     from app.routes.facture_routes import facture_bp
+    from app.routes.depense_routes import depense_bp
 
 
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(produit_bp)
     app.register_blueprint(abonnement_bp)
     app.register_blueprint(facture_bp)
+    app.register_blueprint(depense_bp)
 
 
     with app.app_context():
