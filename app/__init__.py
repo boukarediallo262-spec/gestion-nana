bd8ecc4323b2ec26b0aacc9818f91c4ea8ddb5f4
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-26 14:15:26 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-    db.init_app(app)
+    db.init_app(app) 
 
 
     from app.routes.dashboard_routes import dashboard_bp
