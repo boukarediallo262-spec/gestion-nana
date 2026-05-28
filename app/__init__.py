0728a652c4b1f00622f7a39950eb08a92636e02e
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-28 10:03:04 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
 
-from flask_migrate import Migrate
+
 
 db = SQLAlchemy()
-migrate = Migrate()
+
 def create_app():
 
     app = Flask(__name__)
@@ -18,7 +18,7 @@ def create_app():
 
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    
 
 
     from app.routes.dashboard_routes import dashboard_bp
