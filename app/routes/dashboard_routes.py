bfd0aa9c4381c04e04049039b6b05f7238d18220
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-05-29 09:26:06 +0000

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect
-from app.models.models import Produit, Facture, Depense
+from app.models.models import db, Produit, Facture, Depense
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
