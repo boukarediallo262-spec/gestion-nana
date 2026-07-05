1b5c8693ff64dc99f1a6114266a6c3b7fe16e314
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-07-05 15:13:00 +0000

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+# Import des modèles
+from .user import User
+from .categorie import Categorie
+from .produit import Produit
+from .client import Client
+from .fournisseur import Fournisseur
+from .depense import Depense
+from .facture import Facture, LigneFacture
