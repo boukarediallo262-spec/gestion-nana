f24f9ea0bb4d14d6f40b8d2b7f7fcca7698d0c80
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-07-11 14:48:32 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from app.models import db, User
 
-db = SQLAlchemy()
 login_manager = LoginManager()
-
 
 def create_app():
 
     app = Flask(__name__)
 
-    # ==========================
-    # CONFIGURATION
-    # ==========================
-
     app.config["SECRET_KEY"] = "secret-key-faso-gestion"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    # ==========================
-    # INITIALISATION
-    # ==========================
-
     db.init_app(app)
     login_manager.init_app(app)
 
-    # ==========================
-    # IMPORT DES MODELES
-    # ==========================
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from app import models
-
-    # ==========================
-    # ENREGISTREMENT DES ROUTES
-    # ==========================
 
     from app.routes import init_app
     init_app(app)
