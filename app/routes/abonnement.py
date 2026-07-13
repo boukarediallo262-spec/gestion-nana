6e23a597fa6ead603b5d8ef8fc16d2919d8ad6e8
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-07-13 17:27:37 +0000

    Update abonnement.py

--- app/routes/abonnement.py
+++ app/routes/abonnement.py
@@ -10,4 +10,18 @@ abonnement_bp = Blueprint(
 @abonnement_bp.route("/")
 def index():
 
+    mode_paiement = db.Column(
+        db.String(50),
+        default="Orange Money"
+    )
+
+    reference_paiement = db.Column(
+        db.String(100)
+    )
+
+    date_paiement = db.Column(
+        db.DateTime
+    )
+    
+
     return render_template("abonnement/index.html")
