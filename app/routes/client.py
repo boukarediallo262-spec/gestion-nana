2318d5cdbf97d305a08aac543cd32eb6f004562d
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-07-14 16:13:30 +0000

    Update client.py

--- app/routes/client.py
+++ app/routes/client.py
@@ -5,15 +5,19 @@ from app.models import db, Client
 from flask_login import login_required
 from app.utils.abonnement import abonnement_requis
 
+
+
+client_bp = Blueprint(
+    "client",
+    __name__,
+    url_prefix="/clients"
+)
+
 @client_bp.route("/")
 @login_required
 @abonnement_requis
 def clients():
     ...
-
-client_bp = Blueprint("client", __name__)
-
-
 # ==========================
 # LISTE CLIENTS
 # ==========================
