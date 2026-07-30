8d51ec49e98f0544d6e6092ed8689d069d94d11b
boukarediallo262-spec <boukarediallo262@gmail.com>
2026-07-30 13:56:08 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
+import os
 from app import create_app
+
 print("Création de l'application...")
 
 app = create_app()
+
 print("Application créée :", app)
+
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
